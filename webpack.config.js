936fe83827e3563b0a053ec8ba526dd503e7d3d1
m.kalenishin <m.kalenishin@ylab.io>
2022-08-04 08:08:24 +0300

    fix: edited webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,73 +7,83 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const stylesHandler = "style-loader";
 
 const pages = [
-  "index"
-  // Other html
+    "index",
+    "my-company"
+    // Other html
+];
+
+const pages_ = [
+    "my-company"
+    // Other html
 ];
 
 
 const config = {
-  entry: pages.reduce((config, page) => {
-    config[page] = `./src/${page}.js`;
-    return config;
-  }, {}),
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, "dist"),
-    assetModuleFilename: '[name][ext]',
-  },
-
-  optimization: {
-    splitChunks: {
-      chunks: "all",
+    entry: pages.reduce((config, page) => {
+        config[page] = `./src/${page}.js`;
+        return config;
+    }, {}),
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, "dist"),
+        assetModuleFilename: '[name][ext]',
     },
-  },
 
-  devServer: {
-    open: true,
-    host: "localhost",
-  },
+    optimization: {
+        splitChunks: {
+            chunks: "all",
+        },
+    },
 
-  plugins: [].concat(
-    pages.map(
-      (page) =>
+    devServer: {
+        open: true,
+        host: "localhost",
+    },
+
+    plugins: [].concat(
+        pages_.map(
+            (page) =>
+                new HtmlWebpackPlugin({
+                    template: `./src/pages/${page}.html`,
+                    filename: `${page}.html`,
+                    chunks: [page],
+                    inject: true,
+                })
+        ),
         new HtmlWebpackPlugin({
-          template: `./${page}.html`,
-          filename: `${page}.html`,
-          chunks: [page],
-          inject: true,
-        })
-    )
-  ),
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [stylesHandler, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.css$/i,
-        use: [stylesHandler, "css-loader"],
-      },
-      {
-        test: /\.(js|jsx)$/i,
-        loader: "babel-loader",
-      },
-      {
-        test: /\.(png|svg|gif|woff|woff2|eot|jpg|ttf)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
+            template: "./index.html",
+            filename: "index.html",
+        }),
+    ),
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [stylesHandler, "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.css$/i,
+                use: [stylesHandler, "css-loader"],
+            },
+            {
+                test: /\.(js|jsx)$/i,
+                loader: "babel-loader",
+            },
+            {
+                test: /\.(png|svg|gif|woff|woff2|eot|jpg|ttf)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
 };
 
 module.exports = () => {
-  if (isProduction) {
-    config.mode = "production";
-    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
-  } else {
-    config.mode = "development";
-  }
+    if (isProduction) {
+        config.mode = "production";
+        config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+    } else {
+        config.mode = "development";
+    }
 
-  return config;
+    return config;
 };
